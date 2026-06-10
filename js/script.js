3920b6570771b4ed9b4a31c5936a991c89e30c13
Vladislava Yastremskaya <vladislavayastremskaya@gmail.com>
2026-06-10 15:01:43 +0300

    Add variables for DOM elements in script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const inputAmountElement = document.querySelector('.currency__input');
+const selectFromElement = document.querySelector('.currency__select')[0];
+const selectToElement = document.querySelector('currency__select')[1];
+const outputResultElement = document.querySelector('.currency__output');
+
