c4bb93ce9ba6887f129bf046cd5142bf9c34be87
Vladislava Yastremskaya <vladislavayastremskaya@gmail.com>
2026-06-10 23:16:08 +0300

    feat: add API error handling and bring logic to makeRequest

--- js/script.js
+++ js/script.js
@@ -4,43 +4,29 @@ const selectToElement = document.querySelector('.currency__select--to');
 const outputResultElement = document.querySelector('.currency__output');
 const api = "https://api.frankfurter.dev";
 
-inputAmountElement.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    const fromCurrency = selectFromElement.value;
-    const toCurrency = selectToElement.value;
-    const inputValue = event.target.value;
-
-    fetch(`${api}/v2/rate/${fromCurrency}/${toCurrency}`)
+function makeRequest (fromCurrency, toCurrency, inputValue) {
+  fetch(`${api}/v2/rate/${fromCurrency}/${toCurrency}`)
       .then((response) => response.json())
       .then((data) => {
         let result = (inputValue * data.rate).toFixed(2);
         outputResultElement.value = result;
+      })
+      .catch((error) => {
+        console.error('Ошибка API:', error.message);
+        outputResultElement.value = 'Ошибка';
       });
+}
+
+inputAmountElement.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    makeRequest(selectFromElement.value, selectToElement.value, event.target.value)
   }
 });
 
 selectFromElement.addEventListener('change', () => {
-  const fromCurrency = selectFromElement.value;
-  const toCurrency = selectToElement.value;
-  const inputValue = inputAmountElement.value;
-
-  fetch(`${api}/v2/rate/${fromCurrency}/${toCurrency}`)
-    .then((response) => response.json())
-    .then((data) => {
-      let result = (inputValue * data.rate).toFixed(2);
-      outputResultElement.value = result;
-    });
+  makeRequest(selectFromElement.value, selectToElement.value, inputAmountElement.value)
 });
 
 selectToElement.addEventListener('change', () => {
-  const fromCurrency = selectFromElement.value;
-  const toCurrency = selectToElement.value;
-  const inputValue = inputAmountElement.value;
-
-  fetch(`${api}/v2/rate/${fromCurrency}/${toCurrency}`)
-    .then((response) => response.json())
-    .then((data) => {
-      let result = (inputValue * data.rate).toFixed(2);
-      outputResultElement.value = result;
-    });
+  makeRequest(selectFromElement.value, selectToElement.value, inputAmountElement.value)
 });
