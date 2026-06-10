284cd711df030328ad9bc9a8f88be5c7a333e411
Vladislava Yastremskaya <vladislavayastremskaya@gmail.com>
2026-06-10 16:40:56 +0300

    feat: add function convert to fetch exchange rate from API

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const selectFromElement = document.querySelector('.currency__select--from');
 const selectToElement = document.querySelector('currency__select--to');
 const outputResultElement = document.querySelector('.currency__output');
 
+function convert(base, quote, amount) {
+  const api = "https://api.frankfurter.dev";
+  return fetch(`${api}/v2/rate/${base}/${quote}`)
+    .then((response) => response.json())
+    .then((d) => {
+        let result = (amount * d.rate).toFixed(2);
+        return result
+    });
+}
