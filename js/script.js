ceec98af0a162ec070fdf6d4645ab1255a0d08a5
Vladislava Yastremskaya <vladislavayastremskaya@gmail.com>
2026-06-10 23:08:02 +0300

    feat: auto-update when changing the currency 'From'

--- js/script.js
+++ js/script.js
@@ -18,3 +18,16 @@ inputAmountElement.addEventListener('keydown', (event) => {
       });
   }
 });
+
+selectFromElement.addEventListener('change', () => {
+  const fromCurrency = selectFromElement.value;
+  const toCurrency = selectToElement.value;
+  const inputValue = inputAmountElement.value;
+
+  fetch(`${api}/v2/rate/${fromCurrency}/${toCurrency}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let result = (inputValue * data.rate).toFixed(2);
+      outputResultElement.value = result;
+    });
+});
